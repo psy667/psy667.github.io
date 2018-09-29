62ef99b7f4589fa6ca036aaaadf7560275c805e5
psy667 <arseniyoguzov@gmail.com>
2018-09-29 16:38:48 +0300

    fix function getWeek

--- schedule/script.js
+++ schedule/script.js
@@ -24,9 +24,11 @@ let app = new Vue({
 
     },
     getWeek: function(){
-      let now = new Date;
-      let onejan = new Date(now.getFullYear(), 0, 1);
-      return Math.ceil((((now - onejan) / 86400000) + onejan.getDay() + 1) / 7);
+      let date = new Date();
+      date.setHours(0, 0, 0, 0);
+      date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+      let week1 = new Date(date.getFullYear(), 0, 4);
+      return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000 - 3 + (week1.getDay() + 6) % 7) / 7);
     },
     getMinutes: function(){
       let timers = this.timing['monday'].join(',').split(',');
