b43098cab2ef3afdab7014224e4f838069b573b3
psy667 <arseniyoguzov@gmail.com>
2018-10-06 23:29:05 +0300

    fix sw

--- schedule/sw.js
+++ schedule/sw.js
@@ -2,9 +2,9 @@
 
 importScripts('sw-toolbox.js');
 
-toolbox.precache(["schedule/index.html","schedule/style.css","schedule/script.css"]);
+toolbox.precache(["schedule/","schedule/index.html","schedule/style.css","schedule/script.css"]);
 
-toolbox.router.get('/images/*', toolbox.cacheFirst);
+toolbox.router.get('/*', toolbox.cacheFirst);
 
 toolbox.router.get('/*', toolbox.networkFirst, {
   networkTimeoutSeconds: 5
