73a8d3f8435bb75c76b59f284e02a13c4c91fe7b
psy667 <arseniyoguzov@gmail.com>
2018-10-06 23:07:35 +0300

    fixed service worker

--- schedule/sw.js
+++ schedule/sw.js
@@ -1,34 +1,60 @@
-﻿self.addEventListener('install', e => {
- e.waitUntil(
-   // ïîñëå óñòàíîâêè service worker
-   // îòêðûòü íîâûé êýø
-   caches.open('my-pwa-cache').then(cache => {
-     // äîáàâëÿåì âñå URL ðåñóðñîâ, êîòîðûå õîòèì çàêýøèðîâàòü
-     return cache.addAll([
-       '/schedule',
-       '/schedule/index.html',
-       '/schedule/style.css',
-       '/schedule/script.js',
-       'https://cdn.jsdelivr.net/npm/vue@2.5.17/dist/vue.min.js',
-       'https://unpkg.com/axios/dist/axios.min.js',
-       '/in171.json',
-     ]);
-   })
- );
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/schedule',
+  '/schedule/index.html',
+  '/schedule/style.css',
+  '/schedule/script.js',
+  'https://cdn.jsdelivr.net/npm/vue@2.5.17/dist/vue.min.js',
+  'https://unpkg.com/axios/dist/axios.min.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
 self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
 
-console.log(event.request.url);
+        // IMPORTANT: Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response.
+        var fetchRequest = event.request.clone();
 
-event.respondWith(
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
 
-caches.match(event.request).then(function(response) {
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
 
-return response || fetch(event.request);
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
 
-})
-
-);
-
+            return response;
+          }
+        );
+      })
+    );
 });
